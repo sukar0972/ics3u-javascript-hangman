f8ece86ad92fe20d9bfdf7bebbaef63e1368e074
unknown <sullaimon12@gmail.com>
2023-05-11 22:39:12 -0400

    Adding stuff

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-rect(1,10,10,10);
+
+
+//Show the mouse Position
+function showMousePos() {
+    // Get the HTML element to display the mouse position
+    const mousePosition = document.querySelector('#mouse-position');
+
+    // Add an listener to the document to track mouse movement
+    document.addEventListener('mousemove', function(event) {
+    // Get the mouse position from the event object
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // Update the HTML with the mouse position
+    mousePosition.innerHTML = `Mouse position: (${x}, ${y})`;
+    });
+}
