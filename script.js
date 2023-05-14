b07e0e1a2dc25fdf5bbe3d8d6b901538a0575a26
unknown <sullaimon12@gmail.com>
2023-05-14 01:14:06 -0400

    Menu screen and mouse position

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
+/*S Kargel
+2023/05/14
+Javascript Challenge
+*/
 
+var canvas ;
+var context;
+
+canvas = document.getElementById("canvas");
+    
+let bound = canvas.getBoundingClientRect();
+
+let xOff = bound.left + window.scrollX;
+let yOff = bound.top + window.scrollY;
+
+
+showMousePos();
+
+//Do this when the window first loads
+window.onload = function() {
+
+    
+    canvas.height = 800;
+    canvas.width = 1200;
+
+    context = canvas.getContext("2d");
+
+    mainMenu();
+    showMousePos();
+    
+}
+
+function mainMenu() {
+        //Easy Button
+        context.fillStyle="black";
+        context.fillRect(500,500,200,75);
+        context.fillStyle="white";
+        context.fillRect(500+2,500+2,200-4,75-4);
+        
+        //Hard Button
+    
+        context.fillStyle="black";
+        context.fillRect(500,400,200,75);
+        context.fillStyle="white";
+        context.fillRect(500+2,400+2,200-4,75-4);
+
+        
+}
 
 //Show the mouse Position
 function showMousePos() {
@@ -12,6 +59,7 @@ function showMousePos() {
     const y = event.clientY;
 
     // Update the HTML with the mouse position
-    mousePosition.innerHTML = `Mouse position: (${x}, ${y})`;
+    mousePosition.innerHTML = `Mouse position: (${x-xOff}, ${y-yOff})`;
     });
 }
+
